e5d14e6c70c684e8c7756971301717afa11f94e8
CrayonProof <mickyg1980@gmail.com>
2017-12-16 17:10:47 -0700

    partial fixes for DivisionTester

--- src/packageForTests/DivisionTester.java
+++ src/packageForTests/DivisionTester.java
@@ -3,28 +3,30 @@ package packageForTests;
 public class DivisionTester {
 	
 	public static void main(String[] args) {
-		/*
-		for (int a = 2; a < 91; a++) // numerator a
+		///*
+		for (int a = 3; a < 91; a++) // numerator a
 		{
-			for (int b = 2; b < 91; b++) // denominator b
+			for (int p = 2; p < a; p++) //mod operand
 			{
-				for (int p = 2; p < (a/b); p++) //mod operand
+				for (int b = 3; b < 91; b++) //denominator b
 				{
-					System.out.println("(" + a + " / " + b + ") % " + p + " = " + QuotientOf(a, b, p));
+					//System.out.println("(" + a + " / " + b + ") % " + p + " = " + QuotientOf(a, b, p));
 					//System.out.println("(" + a + " % " + p + " ) " + " / (" + b + " % " + p + ")" + " = " + QuotientOfModInc(a, b, p));
 					if (test(a, b, p))
 					{
-						System.out.println("TRUE");
+						//System.out.println("TRUE");
 					}
 					else
 					{
-						System.out.println("fls");
+						System.out.println("fls : " + a + " : " + b + " : " + p);
+						break;
 					}
 				}
 			}
+			//System.out.println("TRUE");
 		}
-		*/
-		System.out.println(test(8, 4, 3));
+		//*/
+		//System.out.println(test(7, 4, 2));
 	}
 	
 	public static boolean test(int a, int b, int p)
@@ -44,8 +46,16 @@ public class DivisionTester {
 	
 	public static double QuotientOfModInc(int a, int b, int p)
 	{
-		double spor = (a%p) / (b%p);
-		return Math.floor(spor);
+		if (!(((a%p) == 0) || ((b%p) == 0)))
+		{
+			double spor = (a%p) / (b%p);
+			return Math.floor(spor);
+		}
+		else
+		{
+			return QuotientOf( a, b, p);
+		}
+		
 	}
 
 }
